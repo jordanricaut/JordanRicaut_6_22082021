346ecb8a8ba5b276e802ba0a2526d83f86e4fe83
Jordan ricaut <jordan.ricaut5@gmail.com>
2021-09-02 00:47:04 +0200

    Mise en place des middleware d'authentification et du formatage du nom des images

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 
 const userRoutes = require('./routes/user');
@@ -16,11 +17,6 @@ mongoose.connect('mongodb+srv://Admin:AdminBdd56@bddpiiquante.dzozu.mongodb.net/
 
 
 app.use((req, res, next) => {
-   res.json({ message: 'API P6_Piiquante Jordan Ricaut' });
-   next();
-});
-
-app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -28,6 +24,9 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+// Traitement des requêtes vers la route /images
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', userRoutes);
 
