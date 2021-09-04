7b350054d43d505b23abadf6c0f090c3861cbf05
Jordan ricaut <jordan.ricaut5@gmail.com>
2021-09-04 11:36:29 +0200

    Modification du middleware Multer

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -6,14 +6,26 @@ const MIME_TYPES = {
   'image/png': 'png'
 };
 
+
+// Mise en place du formatage du jour
+let date1 = new Date();
+let dateLocale = date1.toLocaleString('fr-FR',{
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+});
+const dateNow = dateLocale.split('/').join('');
+
+
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
+    const nameWithoutExt = name.split('.')[0]
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, nameWithoutExt + '_' + dateNow + '.' + extension);
   }
 });
 
